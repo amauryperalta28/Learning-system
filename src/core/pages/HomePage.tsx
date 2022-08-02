fa2c38ea4a958e8a63dd0a03f51a949d0f4ed5c6
amauryperalta28 <amauryperalta28@gmail.com>
2022-08-02 08:55:21 -0400

    Added tab on home screen

--- src/core/pages/HomePage.tsx
+++ src/core/pages/HomePage.tsx
@@ -1,9 +1,32 @@
+import { Space } from "antd";
+import Tabs from "antd/lib/tabs"
 import { TrainingList as TrainingList } from "../../shared/TrainingList/TrainingList"
+
+const { TabPane } = Tabs;
 
 export const HomePage = () => {
     return (
         <>
-            <TrainingList sectionName="Featured" />
+            <div className="container-fluid">
+                <div className="row">
+                    <TrainingList sectionName="Featured" />
+
+                </div>
+
+                <div className="row">
+
+
+                    <Tabs className="mt-5" defaultActiveKey="1" size="large">
+                        <TabPane tab="TASKS" key="1">
+                            Tasks
+                        </TabPane>
+                        <TabPane tab="DISCOVER" key="2">
+                            Discover
+                        </TabPane>
+                    </Tabs>
+                </div>
+            </div>
+
         </>
 
     )
